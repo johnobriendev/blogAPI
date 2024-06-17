e4ed47a04a39c30bdeb64f6e5e92c8b3748a415f
John O'Brien <johnobrien.dev@gmail.com>
2024-06-17 15:28:37 -0400

    update app.js to include routes

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const dotenv = require('dotenv');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var postsRouter = require('./routes/posts');
+var commentsRouter = require('./routes/comments');
+
+dotenv.config();
 
 var app = express();
+
+//connect to mongodb 
+const mongoDb = process.env.MONGODB_URI;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +30,15 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(passport.initialize()); 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('..controllers/userController');
+const userController = require('../controllers/userController');
 
 router.post('/login', userController.loginUser);
 
